95e1ec00ad8848a07f71bcad1ae6f3708df6c24f
Zycrasion <hendrix.may@gmail.com>
2023-04-07 18:45:21 +1000

    WSL + Windows build works

---
+++ CrossPlatformGLEngine/CrossPlatformGLEngine.cpp
@@ -0,0 +1,48 @@
+﻿// CrossPlatformGLEngine.cpp : Defines the entry point for the application.
+//
+
+#include "CrossPlatformGLEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(720, 480, "GLFW window", NULL, NULL);
+
+	if (!window)
+	{
+		cout << "Failed to create window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialise glad";
+		glfwTerminate();
+		return -1;
+	}
+
+	glClearColor(0.5, 0.25, 0.25, 1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
---
+++ CrossPlatformGLEngine/CrossPlatformGLEngine.h
@@ -0,0 +1,11 @@
+﻿// CrossPlatformGLEngine.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// TODO: Reference additional headers your program requires here.
