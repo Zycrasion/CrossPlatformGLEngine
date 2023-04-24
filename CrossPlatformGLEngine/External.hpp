f4274edca614595fd9935596eccd30fe95256910
Zycrasion <hendrix.may@gmail.com>
2023-04-24 17:00:14 +1000

    moved imgui files

--- CrossPlatformGLEngine/External.hpp
+++ CrossPlatformGLEngine/External.hpp
@@ -4,5 +4,3 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "stb_image.hpp"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
--- tests/imgui_test.cpp
+++ tests/imgui_test.cpp
@@ -1,7 +1,7 @@
 #include <CrossPlatformGLEngine.hpp>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 #include <thread>
 #include <cmath>
 
